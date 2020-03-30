f064c11cf976f4fcd4cfe9e868fc7146d5b5f2a3
dudwns0911 <dudwns0911@naver.com>
2020-03-30 21:46:30 +0900

    rollerpan

--- app/src/main/java/com/example/rollerproject/MainActivity.java
+++ app/src/main/java/com/example/rollerproject/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.rollerproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout screen;
+    ImageView roulette;
+    float startDegree = 0;
+    float endDegree = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        roulette =(ImageView) findViewById(R.id.roulette);
+        screen = (ConstraintLayout) findViewById(R.id.screen);
+        screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotate();
+            }
+        });
+    }
+
+    public void rotate(){
+        startDegree =endDegree;
+
+        Random rand = new Random();
+        int degree_rand = rand.nextInt(360);
+        endDegree = startDegree +360 *10 +degree_rand;
+
+        ObjectAnimator object = ObjectAnimator.ofFloat(roulette,"rotation",startDegree,endDegree);
+        object.setInterpolator(new AccelerateDecelerateInterpolator());
+        object.setDuration(3000);
+        object.start();
     }
 }
